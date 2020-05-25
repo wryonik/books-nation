3aec866cd19ae700151eb2f882dba1e76ca79eb2
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2020-05-26 00:55:47 +0530

    navbar: Made a basic navbar.
    In this commit, some basic directories are formed and made a basic
    navbar.

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return(
+            <div className="header">
+                Book<span className="nation">Nation</span>
+            </div>
+        );
+    }
+}
+
+export default Header;
