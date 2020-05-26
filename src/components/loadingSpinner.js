bc971b0677b968bbeda69fb7a8161fd4d9bea3bc
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2020-05-26 15:32:39 +0530

    spinner: Added loading spinner.
    Added a loading spinner while the data is being fetched.

---
+++ src/components/loadingSpinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoadingSpinner = () => (
+  <div className="spinner">
+    <i className="fa fa-spinner fa-pulse" /> 
+    <p>Fetching Books...</p>
+  </div>
+);
+
+export default LoadingSpinner;
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Searchbar from './../components/searchbar';
 import { getBookByName, getBookByNameOrderBy } from '../api/booksApi';
 import BooksList from '../components/booksList';
+import LoadingSpinner from '../components/loadingSpinner';
 
 class LandingPage extends Component {
     constructor(props) {
@@ -9,7 +10,8 @@ class LandingPage extends Component {
         this.state = {
             books: [],
             searchField: '',
-            sort: 'relevance'
+            sort: 'relevance',
+            isLoading: false
         }
     }
 
@@ -30,8 +32,12 @@ class LandingPage extends Component {
         if(e.target.value!==undefined) {
             await this.setState({sort: e.target.value});
         }
-        getBookByNameOrderBy(this.state.searchField, this.state.sort).then((data) => {
-            this.setState({ books: [...data.items] })
+        this.setState({ isLoading:true }, () => {
+            getBookByNameOrderBy(this.state.searchField, this.state.sort).then((data) => {
+                console.log(this.state.isLoading);
+                this.setState({ isLoading:false, books: [...data.items] })
+                console.log(this.state.isLoading);
+            })
         })
     }
 
@@ -39,7 +45,7 @@ class LandingPage extends Component {
         return(
             <div className="landing">
                 <Searchbar searchBook={this.searchBook} handleSearch={this.handleSearch} handleSort={this.handleSort} sort={this.state.sort} />
-                <BooksList books={this.state.books} />
+                {this.state.isLoading ? <LoadingSpinner /> : <BooksList books={this.state.books} />}
             </div>
         );
     }
