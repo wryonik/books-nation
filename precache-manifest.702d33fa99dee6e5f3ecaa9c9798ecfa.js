bd7da39ba655e2bd7bdcf4868d2a5e0ab49324cc
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2020-05-27 16:47:03 +0530

    Updates

---
+++ precache-manifest.702d33fa99dee6e5f3ecaa9c9798ecfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef62ad222ec35466a5af6257ce51880",
+    "url": "/books-nation/index.html"
+  },
+  {
+    "revision": "092c85425e68cac94619",
+    "url": "/books-nation/static/css/main.cd1207f9.chunk.css"
+  },
+  {
+    "revision": "b0ce6b96a7b495003b84",
+    "url": "/books-nation/static/js/2.fbad6d1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/books-nation/static/js/2.fbad6d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092c85425e68cac94619",
+    "url": "/books-nation/static/js/main.abe2e8bf.chunk.js"
+  },
+  {
+    "revision": "b5571ff33f86790d291a",
+    "url": "/books-nation/static/js/runtime-main.7dc384e1.js"
+  }
+]);
